6c184f3fe8b255c7334a688328f01a4be6030940
“rezan5910” <bytemefirst@gmail.com>
2024-01-04 08:49:08 +0000

    added appropriate event listeners and functions to recive and inoout and output the results after check

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-function checkPalindrome(str) {
-    //need to remove all non-alphanumeric characters (punctuation, spaces and symbols) 
-    const regEx= /[\W_]/g; 
-    const processedStr = str.toLowerCase().replace(regEx, '');
+// function checkPalindrome(str) {
+//     //need to remove all non-alphanumeric characters (punctuation, spaces and symbols) 
+//     const regEx= /[\W_]/g; 
+//     const processedStr = str.toLowerCase().replace(regEx, '');
 
-   let opoStr = processedStr.split('').reverse().join('');
-   if (opoStr === processedStr) {
-      console.log('Palindrome');
-   } 
-   else {
-      console.log('Not Palindrome');
-   }
+//    let opoStr = processedStr.split('').reverse().join('');
+//    if (opoStr === processedStr) {
+//       console.log('Palindrome');
+//    } 
+//    else {
+//       console.log('Not Palindrome');
+//    }
+// }
+
+// checkPalindrome('eye')
+// checkPalindrome('-eye')
+// checkPalindrome('My age is 0, 0 si ega ym.')
+// checkPalindrome('five|\_/|four')
+
+document.getElementById('check-btn').addEventListener('click',()=>{
+    // get the inout value and strip and process it
+    const inputValue = document.getElementById('text-input').value;
+    const cleanedUpInput= cleanUp(inputValue);
+
+    // check if cleaned up value is palindrome
+    const isPalindrome = checkPalindrome(cleanedUpInput)
+
+    // display the result on the page
+    displayResult(inputValue, isPalindrome);
+
+})
+
+// clean uo func to remove non alphanumberic chars and convert to lower case
+function cleanUp(str) {
+    const regEx= /[\W_]/g; 
+    return str.replace(regEx, '').toLowerCase();
 }
 
-checkPalindrome('eye')
-checkPalindrome('-eye')
-checkPalindrome('My age is 0, 0 si ega ym.')
-checkPalindrome('five|\_/|four')
+// palindrome checker func
+
+function checkPalindrome(str) {
+    return str === str.split('').reverse().join('');
+}
+
+// display result func
+
+function displayResult(input, isPalindrome){
+    const resut = document.getElementById('result');
+    const message = isPalindrome ? `Yes, ${input} is a palindrome` : `No, ${input} is not a palindrome`;
+    resut.textContent = message;
+}
